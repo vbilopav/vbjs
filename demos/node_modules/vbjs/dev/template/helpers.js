b2ea1ac3a59ac331b57cfeb7a3c9514f07d89b13
Vedran Bilopavlović <vbilopav@gmail.com>
2018-08-26 19:44:54 +0200

    template context

--- demos/node_modules/vbjs/dev/template/helpers.js
+++ demos/node_modules/vbjs/dev/template/helpers.js
@@ -1,23 +1,25 @@
 define([], () => {
 
-    return {
-        forEach: (obj, template) => (obj instanceof Array ? obj : Object.entries(obj || {})).map(
-            (item, index) => template(...(item instanceof Array ? item : [item]), index)
-        ).join(""),
-
-        import: name => require(name),
-
-        css: {
-            import: (...names) => {
-                document.head.appendChild(
-                    `<style type="text/css">
-                        ${names.map(item => require(item.startsWith("text!") ? item : "text!" + item)).join("")}
-                    </style>`.toHTML()
-                )
-            }
-        },
-
-        if: (condition, templateTrue, templateFalse) => (condition ? templateTrue : templateFalse),
+    return function() {
+        return {
+            forEach: (obj, template) => (obj instanceof Array ? obj : Object.entries(obj || {})).map(
+                (item, index) => template(...(item instanceof Array ? item : [item]), index)
+            ).join(""),
+    
+            import: name => require(name),
+    
+            css: {
+                import: (...names) => {
+                    document.head.appendChild(
+                        `<style type="text/css">
+                            ${names.map(item => require(item.startsWith("text!") ? item : "text!" + item)).join("")}
+                        </style>`.toHTML()
+                    )
+                }
+            },
+    
+            if: (condition, templateTrue, templateFalse) => (condition ? templateTrue : templateFalse),
+        }
     }
 
 });
